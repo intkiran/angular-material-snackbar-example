193f51a27585f6db8ab46c2985b17f3a26d724fc
intkiran <ikiranbabu@gmail.com>
2020-04-30 22:05:18 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,43 @@
-import { Component } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent {
   constructor(private snackBar: MatSnackBar) {}
 
   showSnackbar(content) {
     this.snackBar.open(content);
   }
- showSnackbarAction(content,action) {
-let snack = this.snackBar.open(content, action);
+  showSnackbarAction(content, action) {
+    let snack = this.snackBar.open(content, action);
     snack.afterDismissed().subscribe(() => {
       console.log("This will be shown after snackbar disappeared");
     });
     snack.onAction().subscribe(() => {
       console.log("This will be called when snackbar button clicked");
     });
-}
-showSnackbarDuration(content, action,duration) {
-  this.snackBar.open(content, action,duration);
-
-}
-showSnackbarTopPosition(content, action,duration) {
-      this.snackBar.open(content, action, {
+  }
+  showSnackbarDuration(content, action, duration) {
+    this.snackBar.open(content, action, duration);
+  }
+  showSnackbarTopPosition(content, action, duration) {
+    this.snackBar.open(content, action, {
       duration: 2000,
-      verticalPosition: 'top', // Allowed values are  'top' | 'bottom'
-      horizontalPosition: 'center', // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+      verticalPosition: "top", // Allowed values are  'top' | 'bottom'
+      horizontalPosition: "center" // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
     });
-}
-showSnackbarCssStyles(content, action,duration) {
-let sb = this.snackBar.open(content, action, {
-    duration: duration,
-    panelClass: ['custom-style']
-});
-
-sb.onAction().subscribe(() => {
-    sb.dismiss();
-});
-
-}
+  }
+  showSnackbarCssStyles(content, action, duration) {
+    let sb = this.snackBar.open(content, action, {
+      duration: duration,
+      panelClass: ["custom-style"]
+    });
+    sb.onAction().subscribe(() => {
+      sb.dismiss();
+    });
+  }
 }
