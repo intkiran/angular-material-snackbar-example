c8ec3c9dd000ae3f0590d7ac35dcab737972743b
intkiran <ikiranbabu@gmail.com>
2020-04-30 21:59:49 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,26 @@ export class AppComponent  {
   showSnackbar(content) {
     this.snackBar.open(content);
   }
+ showSnackbarAction(content,action) {
+let snack = this.snackBar.open(content, action);
+    snack.afterDismissed().subscribe(() => {
+      console.log("This will be shown after snackbar disappeared");
+    });
+    snack.onAction().subscribe(() => {
+      console.log("This will be called when snackbar button clicked");
+    });
+}
+showSnackbarDuration(content, action,duration) {
+  this.snackBar.open(content, action,duration);
+
+}
+showSnackbarTopPosition(content, action,duration) {
+      this.snackBar.open(content, action, {
+      duration: 2000,
+      verticalPosition: 'top', // Allowed values are  'top' | 'bottom'
+      horizontalPosition: 'center', // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+    });
+}
 
 
 }
